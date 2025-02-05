71ef18efdee84f273d3fc6b7f824acd1adfb9e45
Pavana Paradesi <pp318@student.le.ac.uk>
2025-02-05 10:49:51 +0530

    updated

--- main.tf
+++ main.tf
@@ -61,21 +61,21 @@ resource "aws_security_group" "my-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
